cc598c1730623cea98b2fd17a4dd6eea3939afa3
Prasang Singhal <prasang.singhal@gmail.com>
2022-03-05 15:23:12 +0530

    Added user profile feed api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,8 +7,10 @@ router.register('hello-viewset', views.HelloViewSet,
                 basename='hello-viewset')
 # basename is not required as DRF will figure out from queryset
 router.register('profiles', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import filters
@@ -101,3 +104,21 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """ Handle creating user authentication tokens """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """ Handles creating, reading and updating profile feed items """
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus,
+                          IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """ Sets the user profile to the logged-in user """
+        serializer.save(user_profile=self.request.user)
